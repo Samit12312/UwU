163e1f75553039840bcee28d1ecceeecbf63070d
Owner <Owner@660084-COMPN-09>
2025-05-08 15:23:26 +0300

    gfhfg

--- NiggerScript/Jarvies.js
+++ NiggerScript/Jarvies.js
@@ -21,11 +21,76 @@ function Targil2() {
     document.getElementById("container").innerHTML = html;
 
 }
+function LohachKefel() {
+    let html = "<div class=matrix>";
+    for (i = 1; i <= 10; i++) {
+        html += "<div class=matrixRow>";
+        for (j = 1; j <= 10; j++) {
+            if ((j + i) % 2 != 0) {
+                html += `<div class='matrixCell zogi'>${i * j}</div>`;
+            }
+            else {
+                html += `<div class='matrixCell ezogi'>${i * j}</div>`;
+            }
+        }
+        html += "</div>";
+    }
+    html += "</div>";
+    document.getElementById("container").innerHTML = html;
+}
+function Targil1() {
+    let num = 10;
+    let html = "<div class=triangle>";
+    for (i = 1; i <= num; i++) { // rows
+        if (i % 2 == 0) {
+            html += "<div class=traingleRowzogi>";
+        }
+        else
+            html += "<div class=traingleRowezogi>";
+
+        for (j = 1; j <= i; j++) { // cell
+            html += "*";
+        }
+        html += "</div>";
+    }
+
+    html += "</div>";
+    document.getElementById("container").innerHTML = html;
+}
+function Targil2() {
+    let num = 10;
+    let num1 = num;
+    let html = "<div class=triangle>";
+    for (i = 1; i <= num; i++) { // rows
+        if (i % 2 == 0) {
+            html += "<div class=traingleRowzogi1>";
+        }
+        else
+            html += "<div class=traingleRowezogi1>";
+
+        for (j = 1; j <= num1; j++) { // cell
+            html += "*";
+        }
+        num1--;
+        html += "</div>";
+    }
+
+    html += "</div>";
+    document.getElementById("container").innerHTML = html;
+}
 let html = "<div class=matrix>";
 for (i = 1; i <= 10; i++) {
     html += "<div class=matrixRow>";
     for (j = 1; j <= 10; j++) {
-        html += `<div class=matrixCell>${i * j}</div>`;
+        if (j == 1) {
+            html += `<div class='matrixCell kkk'>${i * j}</div>`;
+        }
+        else {
+            if (i == 1)
+                html += `<div class='matrixCell kkk'>${i * j}</div>`;
+            else
+            html += `<div class='matrixCell ezogi'>${i * j}</div>`;
+        }
     }
     html += "</div>";
 }
